ef1fc792e6099bcbe5b816c613bab77769890962
U-SURREY\tc0008 <tc0008@UWS30719.surrey.ac.uk>
2019-01-02 16:58:42 +0000

    Separate state and parameter estimation capability. This allows different time horizons used for state and parameter estimation. Demo modified as well for the fedbatch bioreactor.

--- mhe_w.m
+++ mhe_w.m
@@ -15,7 +15,8 @@ function [x, p, f_obj] = mhe_w(xseq, useq, yseq, p, fn_state, fn_obsv, Cov_x, Co
 %   Cov_x, Cov_y, Cov_p -- vectors, the diagonal of covariance matrices of 
 %                           state, measurement and parameters, respectively
 %   t_sample -- sampling interval of the system
-%   bSnP -- binary; 1: both states and parameters; 0: only states to be updated
+%   bSnP -- integer; 
+%       1: both states and parameters; 0: only states to be updated; -1: only parameters to be updated
 %
 % Outputs:
 %   x -- estimated state sequence; nt by nx
@@ -26,12 +27,16 @@ if nargin < 11
     bSnP = 0;
 end
 
+% Optimiser's options; use the 2nd line for no display of information
+opt = optimset('Display', 'iter', 'MaxFunEvals', 5e4, 'MaxIter', 5e4, 'TolX', 1e-8);    
+%opt = optimset('Display', 'off', 'MaxFunEvals', 5e4, 'MaxIter', 5e4, 'TolX', 1e-8);    
+
 [nt, nx] = size(xseq);
 np = length(p);
 
 w0 = zeros(nt-1,nx); %initially assume no state function noise
 
-if bSnP % estimate both state and parameters   
+if bSnP == 1 % estimate both state and parameters   
 
     % organise states and parameters into a single vector for optimisation    
     x_w_p0 = [xseq(1,:)'; reshape(w0, (nt-1)*nx, 1); p'];
@@ -40,13 +45,9 @@ if bSnP % estimate both state and parameters
     x0 = xseq(1,:);
     x0Q = Cov_x; % this is a crude estimate; probably should use EKF to approximate the prior distribution
     p0 = p;
-    
-    opt = optimset('Display', 'iter', 'Algorithm', 'sqp', 'MaxFunEvals', 5e3, ...
-        'MaxIter', 5e2, 'TolX', 1e-10, 'TolFun', 1e-4);
-    %[x_w_p1, f_obj] = fmincon(@(x_w_p) mhe_obj(x_w_p, nx, x0, x0Q, p0, Cov_p, useq, yseq, fn_state, Cov_x, ...
-    %    fn_obsv, Cov_y, t_sample), x_w_p0, [], [], [], [], [], [], [], opt);
+        
     [x_w_p1, f_obj] = fminunc(@(x_w_p) mhe_obj(x_w_p, nx, x0, x0Q, p0, Cov_p, useq, yseq, fn_state, Cov_x, ...
-        fn_obsv, Cov_y, t_sample), x_w_p0);
+        fn_obsv, Cov_y, t_sample), x_w_p0, opt);
     
     x = xseq;
     x(1,:) = x_w_p1(1:nx)';
@@ -59,7 +60,7 @@ if bSnP % estimate both state and parameters
         x(i+1,:) = xx;
     end    
     
-else
+elseif bSnP == 0 % estimate states only
 
     % organise states into a single vector for optimisation
     x_w0 = [xseq(1,:)'; reshape(w0, (nt-1)*nx,1)];   
@@ -67,12 +68,8 @@ else
     x0 = xseq(1,:);
     x0Q = Cov_x; % this is a crude estimate; probably should use EKF to approximate the prior distribution
 
-    opt = optimset('Display', 'iter', 'Algorithm', 'interior-point', 'MaxFunEvals', 5e4, ...
-        'MaxIter', 5e4, 'TolX', 1e-8);
-    %[x_w1, f_obj] = fmincon(@(x_w) mhe_obj_states(x_w, p, nx, x0, x0Q, useq, yseq, ...
-    %    fn_state, Cov_x, fn_obsv, Cov_y, t_sample), x_w0, [], [], [],  [], [], [], [], opt);
     [x_w1, f_obj] = fminunc(@(x_w) mhe_obj_states(x_w, nx, x0, x0Q, p, useq, yseq, fn_state, Cov_x, ...
-        fn_obsv, Cov_y, t_sample), x_w0);
+        fn_obsv, Cov_y, t_sample), x_w0, opt);
     
     x = xseq;
     x(1,:) = x_w1(1:nx)';
@@ -83,6 +80,24 @@ else
         x(i+1,:) = xx;
     end
     
+elseif bSnP == -1 % estimate parameters only
+ 
+    % organise states and parameters into a single vector for optimisation    
+    p0 = p';
+    x0 = xseq(1,:);    
+        
+    [p1, f_obj] = fminunc(@(p) mhe_obj_paras(p, nx, x0, useq, yseq, fn_state, fn_obsv, Cov_y, t_sample), p0, opt);
+    
+    x = xseq;    
+    p = p1;
+    for i = 1 : nt-1,
+        ui = useq(i, :);
+        xx = feval(fn_state, x(i,:), p, ui, t_sample);
+        x(i+1,:) = xx;
+    end
+        
+else % error message
+    error('bSnP must be 0, 1, or -1');
 end
 
 
@@ -200,6 +215,42 @@ c_obsv = c_obsv/2;
 
 
 f_mhe = c_arrv + c_proc + c_obsv;
+
+if ~isreal(f_mhe)
+    error('not real value of the objective function');
+end
+
+return;
+
+
+%% Internal function to calculate objective function to be minimised
+%   by adjusting only the parameters
+function f_mhe = mhe_obj_paras(p, nx, x0, useq, yseq, fn_state, fn_obsv, Cov_y, t_sample)
+                     
+[nt, ny] = size(yseq);
+
+log_2pi = log(2*pi);
+
+% cost regarding measurement
+c_obsv = 0;
+x = x0;
+for i = 1 : nt    
+    ypred = feval(fn_obsv, x, p);
+    er = ypred - yseq(i,:);
+    c_obsv = c_obsv + sum( (er.^2) ./ Cov_y );
+    
+    if i == nt
+        break;
+    end
+    
+    % then move on to the next time step    
+    ui = useq(i, :);
+    x = feval(fn_state, x, p, ui, t_sample);
+end
+c_obsv = c_obsv + ny*nt*log_2pi + nt*sum(log(Cov_y));
+c_obsv = c_obsv/2;
+
+f_mhe = c_obsv;
 
 if ~isreal(f_mhe)
     error('not real value of the objective function');
